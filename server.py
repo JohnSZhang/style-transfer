e52324b342db93d7ace2f05a48c65d022dee08da
Shimin Zhang <john.shimin.zhang@gmail.com>
2018-04-02 06:36:30 -0400

    add model code, add inference ability for site

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+from model.code.main.load_model_demo import PreProcessing as prepro
+from model.code.main.load_model_demo import Demo
 
 app = Flask(__name__)
 
+demo = Demo()
+demo.loadModel("data/pointer_model7.ckpt")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/inference', methods=['POST'])
+def inference():
+    if request.method == 'POST':
+        text_input = request.form['text']
+        text_input = text_input.split("\n")
+        print 'new text input:', text_input
+        return demo.getOutput(text_input)
 
 
 
---
+++ static/js/stylize.js
@@ -0,0 +1,54 @@
+console.log("stylize script loaded");
+$(function() {
+
+  $("#input input, #input textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var message = $("textarea#message").val();
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "./inference",
+        type: "POST",
+        data: {
+          text: message
+        },
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry it seems that stylize server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
